df26ade46879030883b0fc9ef706bb4aee3e5bd6
50bit <omar.gamal1015@gmail.com>
2023-08-29 00:05:12 +0300

    add create update and delete for delivery zones

--- src/modules/delivery-zones/controllers/delivery-zones.controller.ts
+++ src/modules/delivery-zones/controllers/delivery-zones.controller.ts
@@ -12,22 +12,27 @@ import {
     Query,
     HttpCode,
     Headers,
+    Delete,
 } from '@nestjs/common';
 import { DeliveryZonesService } from '../services/delivery-zones.service';
 import { ApiBearerAuth, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-import { aggregationPipelineConfig } from 'src/modules/delivery-zones/schemas/delivery-zones.schema';
+import { aggregationPipelineConfig } from 'src/modules/city/schemas/cities.schema';
 import { aggregationMan } from 'src/common/utils/aggregationMan.utils';
 import { CrudController } from 'src/common/crud/controllers/crud.controller';
 import { searchBody } from 'src/common/crud/interfaces/searchBody.dto';
-
-import { clone } from 'lodash'
-import { ObjectIdType } from 'src/common/utils/db.utils';
-
+import {clone} from 'lodash'
+import { deliverZones } from '../interfaces/delivery-zones.dto';
 @ApiTags('Delivery Zones')
 @Controller('delivery-zones')
 export class DeliveryZonesController  {
     constructor(public readonly deliveryZonesService: DeliveryZonesService) {
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.OK)
+    async create(@Body() body: deliverZones) {
+        return await this.deliveryZonesService.create(body);
     }
 
     @Post("search")
@@ -41,5 +46,15 @@ export class DeliveryZonesController  {
         const pipelineConfig = aggregationPipelineConfig(lang)
         const pipeline = aggregationMan(pipelineConfig, search, options)
         return await this.deliveryZonesService.aggregate(pipeline);
+    }
+
+    @Put(':id')
+    async put(@Param('id') id: string, @Body() body: any) {
+        return await this.deliveryZonesService.updateById(id, body);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+        return await this.deliveryZonesService.delete(id);
     }
 }
