6b704195284191f6e78be0b15cb3c89e36730321
50bit <omar.gamal1015@gmail.com>
2023-10-21 14:52:06 +0300

    fix cart search

--- src/modules/drug/controllers/drugRequest.controller.ts
+++ src/modules/drug/controllers/drugRequest.controller.ts
@@ -150,6 +150,7 @@ export class DrugRequestController {
         }
 
         const pipelineConfig = aggregationPipelineConfig(lang)
+        if(search.recieved) search['status'] = 'approved'
         const pipeline = aggregationMan(pipelineConfig, search,options)
         if(search.recieved){
             const pharmacyId = req.user.pharmacyId
